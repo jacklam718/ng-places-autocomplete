22e5c70021f8aa16d8d7aaf85869584c48fff932
jack lam <jacklam718@gmail.com>
2015-08-10 23:32:08 +0800

    code refactor & add map popup window

--- src/fancy-places-autocomplete.js
+++ src/fancy-places-autocomplete.js
@@ -1,103 +1,152 @@
-// register
-angular.module('fancy-places-autocomplete', []);
-
-// tooltip directive
-(function () {
-  'restrict';
-
-  // tooltip directive integrate with tooltipster
-  function Tooltip () {
-    var self = this;
-
-    self.controller = function ($scope) {
-      self.$scope = $scope;
-    };
-
-    self.link = function (scope, elem, attr) {
-      var $elem = $(elem);
-      $elem.addClass('tooltip fancy-tooltip');
-      $elem.tooltipster({
-        animation: 'grow',
-        delay: 200,
-        theme: 'tooltipster-default',
-        touchDevices: false,
-        trigger: 'hover'
-      });
-    };
-
-    return self;
-  };
-
-  // regist tooltip directive
-  angular.module('fancy-places-autocomplete').directive('tooltip', Tooltip);
-})();
-
 // places autocomplete directive
 (function () {
   'restrict';
 
-  function GoogleAutocompleteService (elem) {
+  // regist module
+  angular.module('fancy-places-autocomplete', []);
+
+  function constructQueryData(data) {
+    var query = [];
+    for (var d in data)
+      query.push(d + '=' + data[d])
+    return query.join('&');
+  }
+
+  // base class of map service
+  function BaseMapService () {};
+
+  function getStaticMapQueryUrl (query) {
+    if (this.staticMapApiUrl === undefined) {
+      throw('without static map apt url');
+      return;
+    }
+
+    var defaultQuery = {
+      zoom: 16,
+      maptype: 'roadmap'
+    };
+
+    return this.staticMapApiUrl + '?' + constructQueryData(angular.extend({}, defaultQuery, query));
+  }
+
+  // ****************
+  // Google
+  // ****************
+  function GoogleMapService () {
     var self = this;
-    var autocomplete = new google.maps.places.Autocomplete(elem[0]);
-    var placesService = new google.maps.places.PlacesService(elem[0]);
+    self.staticMapApiUrl = 'https://maps.googleapis.com/maps/api/staticmap';
 
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      var place = autocomplete.getPlace();
-      placesService.getDetails({reference: place.reference}, function (placeResult) {
-        self.onSearchComplete(placeBuilder(placeResult));
+    self.init = function (elem, onSearchComplete) {
+      self.elem = elem;
+      self.onSearchComplete = onSearchComplete;
+      self.autocomplete = new google.maps.places.Autocomplete(elem[0]);
+      self.placesService = new google.maps.places.PlacesService(elem[0]);
+
+      self.initAutocomplete();
+    }
+
+    self.initAutocomplete = function () {
+      google.maps.event.addListener(self.autocomplete, 'place_changed', function() {
+        var place = self.autocomplete.getPlace();
+        self.placesService.getDetails({reference: place.reference}, function (placeResult) {
+          self.onSearchComplete(self.placeBuilder(placeResult));
+        });
       });
-    });
+    };
 
-    var placeBuilder = function (placeResult) {
+    self.placeBuilder = function (placeResult) {
       var place = {};
       place.formatted_address = placeResult.formatted_address;
       place.location = {lat: placeResult.geometry.location.G, lon: placeResult.geometry.location.K};
       return place;
     };
+
+    self.getStaticMapQueryUrl = function (query) {
+      var defaultOpts = {size: '400x400'};
+      return getStaticMapQueryUrl.call(this, angular.extend({}, defaultOpts, query));
+    };
+
+    return self;
   };
 
-  function BaiduAutocompleteService (elem, city) {
+  // ****************
+  // Baidu
+  // ****************
+  function BaiduMapService () {
     var self = this;
-    var autocomplete = new BMap.Autocomplete({input: elem[0], location: city});
-    var $elem = elem[0];
+    self.staticMapApiUrl = 'http://api.map.baidu.com/staticimage';
 
-    autocomplete.addEventListener('onconfirm', function(selectedPlace) {
-      var val = $elem.value;
-      var search = new BMap.LocalSearch(val, {onSearchComplete: function (placeResult) {
-        self.onSearchComplete(placeBuilder(placeResult, selectedPlace));
-      }});
+    self.init = function (elem, city, onSearchComplete) {
+      self.elem = elem;
+      self.city = city;
+      self.onSearchComplete = onSearchComplete;
+      self.autocomplete = new BMap.Autocomplete({input: elem[0], location: city});
+      console.log(self.elem)
+      self.initAutocomplete();
+    }
 
-      search.search(val);
-    });
+    self.initAutocomplete = function () {
+      self.autocomplete.addEventListener('onconfirm', function(selectedPlace) {
+        console.log(self.elem.value)
+        var val = self.elem.value;
+        var search = new BMap.LocalSearch(val, {onSearchComplete: function (placeResult) {
+          self.onSearchComplete(self.placeBuilder(placeResult, selectedPlace));
+        }});
 
-    var placeBuilder = function (placeResult, selectedPlace) {
+        search.search(val);
+      });
+    }
+
+    self.placeBuilder = function (placeResult, selectedPlace) {
       var place = {};
       place.formatted_address = [selectedPlace.item.value.city, selectedPlace.item.value.district].join();
       place.location = {lat: placeResult.getPoi(0).point.lat, lon: placeResult.getPoi(0).point.lng};
       return place;
     };
+
+    self.getStaticMapQueryUrl = function (query) {
+      var defaultOpts = {width: 400, height: 400};
+      return getStaticMapQueryUrl.call(this, angular.extend({}, defaultOpts, query));
+    };
+
+    return self;
   };
 
   // places autocomplete directive
-  function PlacesAutocomplete () {
+  function PlacesAutocomplete (GoogleMapService, BaiduMapService) {
     var self = {};
-
     var serviceProvider = {
-      google: GoogleAutocompleteService,
-      baidu: BaiduAutocompleteService
+      google: GoogleMapService,
+      baidu: BaiduMapService
     };
 
+    self.require = 'ngModel';
     self.scope = {
       city: '=?',
       service: '=?',
+      mapPopup: '=?',
+      mapPopupConfig: '=?',
       ngModel: '='
     };
 
     self.onSearchComplete = function (processedPlace) {
-      self.$scope.$apply(function() {
-        self.$scope.ngModel = processedPlace;
+      self.$scope.$apply(function () {
+        self.scope.ngModel = processedPlace;
         self.$scope.$emit('places-autocomplete:select', processedPlace);
       });
+    };
+
+    self.getStaticMap = function (origin, content) {
+      if (typeof self.scope.ngModel === 'object' && self.scope.ngModel.location.lat && self.scope.ngModel.location.lon) {
+        var lat = self.scope.ngModel.location.lat, lon = self.scope.ngModel.location.lon;
+        var latLon = [lat, lon].join(',');
+        var url = self.provider.getStaticMapQueryUrl({center: latLon});
+        var $imageElement = angular.element('<img>');
+
+        $imageElement.attr({src: url});
+        origin.tooltipster('content', $imageElement);
+      }
+      content();
     };
 
     self.controller = function ($scope) {
@@ -105,18 +154,27 @@ angular.module('fancy-places-autocomplete', []);
     };
 
     self.link = function (scope, elem, attr) {
-      console.log(scope.service);
-      scope.$watch('city', function(oldVal, newVal) {
-        self.provider = serviceProvider[scope.service];
+      self.provider = serviceProvider[scope.service];
+
+      // popup map
+      if (scope.mapPopup || elem.attr('map-popup') !== undefined) {
+        var $elem = $(elem);
+        var popMapDefaultConfig = {animation: 'grow', delay: 200, theme: 'tooltipster-light', touchDevices: false, trigger: 'hover', functionBefore: self.getStaticMap};
+        $elem.addClass('tooltip fancy-tooltip');
+        $elem.tooltipster(angular.extend({}, popMapDefaultConfig, scope.mapPopupConfig));
+      }
+
+      // watch
+      scope.$watch('city', function (oldVal, newVal) {
         switch (scope.service) {
           case 'google':
-            self.provider.call(self, elem);
+            self.provider.init(elem, self.onSearchComplete);
             break;
           case 'baidu':
-            self.provider.call(self, elem, scope.city);
+            self.provider.init(elem, scope.city, self.onSearchComplete);
             break
         }
-      })
+      });
     };
 
     return self;
@@ -124,4 +182,6 @@ angular.module('fancy-places-autocomplete', []);
 
   // regist places autocomplete directive
   angular.module('fancy-places-autocomplete').directive('placesAutocomplete', PlacesAutocomplete);
+  angular.module('fancy-places-autocomplete').factory('GoogleMapService', GoogleMapService);
+  angular.module('fancy-places-autocomplete').factory('BaiduMapService', BaiduMapService);
 })();
