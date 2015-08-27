329c860caf13a46e180f3550254b7fa9fc28253f
jack lam <jacklam718@gmail.com>
2015-08-28 02:59:47 +0800

    rename build/ng-places-autocomplete -> build/ng-places-autocomplete.min

--- Gulpfile.js
+++ Gulpfile.js
@@ -21,14 +21,14 @@ gulp.task('scripts', ['clear'], function() {
   return gulp.src(paths.scripts)
     .pipe(ngAnnotate())
     .pipe(uglify())
-    .pipe(concat('ng-places-autocomplete.js'))
+    .pipe(concat('ng-places-autocomplete.min.js'))
     .pipe(gulp.dest(dest));
 });
 
 gulp.task('stylesheets', ['clear'], function() {
   return gulp.src(paths.stylesheets)
     .pipe(uglifycss())
-    .pipe(concat('ng-places-autocomplete.css'))
+    .pipe(concat('ng-places-autocomplete.min.css'))
     .pipe(gulp.dest(dest));
 })
 
